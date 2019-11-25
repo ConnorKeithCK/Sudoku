1b2ac0c3e73d1be89b0338ef3a7f9272dae51718
Keith <keithcs@mail.uc.edu>
2019-11-25 13:04:56 -0500

    further game logic

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ TODO:
 let game_board = document.getElementById("board");
 let play_button = document.getElementById("button-play");
 let validate_button = document.getElementById("button-validate");
+let solve_button = document.getElementById("button-solve");
 let inputs = game_board.getElementsByTagName("input");
+let game;
 
-let boards = 
+
+
+class Sudoku {
+
+    boards = 
     [
         ["","","9","","","","4","",""],
         ["","","","","6","4","","5",""],
@@ -25,7 +31,7 @@ let boards =
         ["","","6","","","","1","",""]
     ]
 
-let solution = 
+   solution = 
     [
     ["3","6","9","1","2","5","4","7","8"],
     ["1","8","7","9","6","4","2","5","3"],
@@ -38,15 +44,72 @@ let solution =
     ["8","9","6","2","5","3","1","4","7"]
     ]
 
-
-class Sudoku {
+    user_board = [[],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []]
     
     constructor() {
         for (var i = 0; i < game_board.rows.length; i++) {
             for (var x = 0; x < game_board.rows[i].cells.length; x++) {
-                if (boards[i][x] != "") {
-                    game_board.rows[i].cells[x].innerHTML = boards[i][x];
+                if (this.boards[i][x] != "") {
+                    game_board.rows[i].cells[x].innerHTML = this.boards[i][x];
                 }
+            }
+        }
+    }
+
+    clearBoard() {
+        this.user_board.length = 0;
+    }
+
+    solvePuzzle() {
+         
+      for (var r = 0; r < game_board.rows.length; r++) {
+          for (var c = 0; c < game_board.rows[r].cells.length; c++) {
+              if (this.boards[r][c] != "") {
+                    this.user_board[r][c] = game_board.rows[r].cells[c].innerHTML;
+              } else {
+                    this.user_board[r][c] = game_board.rows[r].cells[c].children[0].value;
+              }
+          }
+      }
+
+    }
+
+    compareBoards() {
+
+        // Gather all user inputs to the user_board array
+        for (var i = 0; i < game_board.rows.length; i++) {
+            for (var x = 0; x < game_board.rows[i].cells.length; x++) {
+                if (this.boards[i][x] != "") {
+                    this.user_board[i][x] = game_board.rows[i].cells[x].innerHTML;
+                } else {
+                    this.user_board[i][x] = game_board.rows[i].cells[x].children[0].value;
+                }
+            }
+        }
+    
+    
+        for (var x = 0; x < this.user_board.length; x++) {
+            for (var y = 0; y < this.user_board[x].length; y++) {
+                if (this.user_board[x][y] == this.solution[x][y]) {
+                    if (confirm("You have won, would you like to play again?")) {
+                        this.clearBoard();
+                        return true;
+                    } else {
+                        // they won, but no longer want to play
+                    }
+                    
+                } else {
+                    // Change color of wrong cells
+                    return false;
+                }        
             }
         }
     }
@@ -54,35 +117,24 @@ class Sudoku {
 
 validate_button.addEventListener("click", function(clickEvent) {
 
-    var user_board = [[],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []]
+if (game.compareBoards()) {
+    alert("You have won!");
+    
+} else {
+    alert("Incorrect!");
+}
+    
+});
 
+solve_button.addEventListener("click", function(clickEvent) {
 
-    for (var i = 0; i < game_board.rows.length; i++) {
-        for (var x = 0; x < game_board.rows[i].cells.length; x++) {
-            if (boards[i][x] != "") {
-                user_board[i][x] = game_board.rows[i].cells[x].innerHTML;
-            } else {
-                user_board[i][x] = game_board.rows[i].cells[x].children[0].value;
-            }
-        }
-    }
-
-    console.log(user_board);
-    console.log(solution);
-
-    // TODO: Iterate through each cell and highlight/modify color of those incorrect instead of checking for full solution and make sure board is full before allowing validation
-    if (user_board === solution) {
-        alert("You won!");
-    }
+game.solvePuzzle();
+console.log("You cheater!");
 
 });
 
-let game = new Sudoku();
+play_button.addEventListener("click", function(clickEvent) {
+
+    game = new Sudoku();
+
+});
